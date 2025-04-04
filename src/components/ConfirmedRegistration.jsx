9f3f851fb4dc3f1f231fbd0f5daa464aad755bd3
Husain-Althagafi <husain.a.althagafi@okaz.com>
2025-04-05 00:39:26 +0300

    fixed some errors and changed styling to better fit the current one

--- src/components/ConfirmedRegistration.jsx
+++ src/components/ConfirmedRegistration.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/EnrollConfirmation.css';
 
-function ConfirmedRegistrationn() {
-  const navigate = useNavigate();
+function ConfirmedRegistration() {
+  
 
   return (
     <section className="confirmed-registration">
       <h1>Registration Confirmed!</h1>
       <h2>Your enrollment has been successfully processed. We look forward to seeing you!</h2>
-      <button onClick={() => navigate('/')}>Return to Home</button>
+      <button>Return to Home</button>
     </section>
   );
 }
