663c686ecb25d83391a33b5b444c640ff1d34c66
Husain-Althagafi <husain.a.althagafi@okaz.com>
2025-05-07 02:21:40 +0300

    a

--- backend/src/app.js
+++ backend/src/app.js
@@ -59,12 +59,3 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
-//Error handling
-app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).json({error: err.message || 'Something went wrong!'})
-})
-
-
-// Export the app instead of calling listen
-module.exports = app;
