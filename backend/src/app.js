83bd5195fd1cc2a75413ab07a56594666132927c
Husain-Althagafi <husain.a.althagafi@okaz.com>
2025-05-07 02:45:13 +0300

    aa

--- backend/src/app.js
+++ backend/src/app.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../build')));
   
   // Handle React routing - must come last
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../build', 'index.html'));
   });
 }
